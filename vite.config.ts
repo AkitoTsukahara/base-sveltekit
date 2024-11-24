df4ca2ca45cdd89824e41ced65f7407fbf3833a2
a-tsukahara <a-tsukahara@MAC28.local>
2024-11-25 01:39:02 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
+		fs: {
+			allow: ['.']
+		}
+	},
+	plugins: [
+		sveltekit(),
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
+	],
+
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
